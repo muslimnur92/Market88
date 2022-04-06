adb034ddc6aa0ca4d8ae605ae3460e0deed78e2a
muslimnur <muslimnur69@yahoo.com>
2022-04-06 23:44:33 +0700

    edisi 7

--- app/src/main/java/com/example/market88/NavigationActivity.kt
+++ app/src/main/java/com/example/market88/NavigationActivity.kt
@@ -34,7 +34,7 @@ class NavigationActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navView.setOnItemSelectedListener {
 
